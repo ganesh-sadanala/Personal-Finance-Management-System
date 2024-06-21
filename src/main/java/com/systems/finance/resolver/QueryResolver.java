6f563dc46c8a7e4822cb00616433cc31c567ce2d
gsadanala <ganesh8374387106@gmail.com>
2024-06-21 01:28:43 -0400

    Implement pagination for expenses

--- src/main/java/com/systems/finance/resolver/QueryResolver.java
+++ src/main/java/com/systems/finance/resolver/QueryResolver.java
@@ -3,6 +3,10 @@ package com.systems.finance.resolver;
 import com.systems.finance.model.*;
 import com.systems.finance.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
@@ -50,5 +54,12 @@ public class QueryResolver {
     @QueryMapping
     public List<Investment> getInvestments(@Argument Long userId) {
         return investmentRepository.findAll();
+    }
+
+    @QueryMapping
+    public Page<Expense> getExpensesPaginated(@Argument Long userId, @Argument int page, @Argument int size,
+                            @Argument String sortBy){
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return expenseRepository.findByUserId(userId, pageable);
     }
 }
